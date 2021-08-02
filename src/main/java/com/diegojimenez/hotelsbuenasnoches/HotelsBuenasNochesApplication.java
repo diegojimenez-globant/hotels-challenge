1aee3a2a7fd01df3c71038117de690b4d9f255a1
diegojimenez <diegouriel.jimenez@globant.com>
2021-08-02 12:04:52 -0500

    start project

---
+++ src/main/java/com/diegojimenez/hotelsbuenasnoches/HotelsBuenasNochesApplication.java
@@ -0,0 +1,13 @@
+package com.diegojimenez.hotelsbuenasnoches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsBuenasNochesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelsBuenasNochesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/diegojimenez/hotelsbuenasnoches/HotelsBuenasNochesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegojimenez.hotelsbuenasnoches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelsBuenasNochesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
